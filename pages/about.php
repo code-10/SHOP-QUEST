73edbbe8a0d6fa7105c607cf104ca5dbda4c8f69
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 17:09:16 +0530

    Create about.php

---
+++ pages/about.php
@@ -0,0 +1,43 @@
+<?php include '../header.php'; ?>
+
+<body>
+   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a href="index.php" class="navbar-brand">ShopShop</a>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+         <div class="navbar-nav">
+            <a href="index.php" class="nav-item nav-link">Home</a>
+            <a href="#" class="nav-item nav-link active">About</a>
+            <a href="#" class="nav-item nav-link">Products</a>
+         </div>
+         <div class="navbar-nav ml-auto">
+            <!--<a href="register/register.php" class="nav-item nav-link">Register</a>
+               <a href="login/login.php" class="nav-item nav-link">Login</a>&nbsp;&nbsp;-->
+            <?php if(isset($_SESSION['user_name'])) {
+               echo '<a href="profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
+               echo '<a href="product/cart_display.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
+               echo '<a href="login/logout.php" class="nav-item nav-link">Logout</a>';
+               }
+               else{
+               echo '<a href="register/register.php" class="nav-item nav-link">Register</a>
+                       <a href="login/login.php" class="nav-item nav-link">Login</a>&nbsp;&nbsp;';
+               }
+               ?>
+         </div>
+         <!--<form class="form-inline">
+            <input type="text" class="form-control mr-sm-2" placeholder="Search" aria-label="search">
+            <button type="submit" class="btn btn-light my-sm-0">Search</button>
+            </form>-->
+      </div>
+   </nav>
+   <div class="jumbotron">
+      <div class="text-center">
+         <p class="display-3">About</p>
+      </div>
+   </div>
+</body>
+  
+  
+  <?php include '../footer.php'; ?>
