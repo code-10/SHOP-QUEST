40e3a84c93f2530bf4d5f497ec30091adff442b1
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 17:12:22 +0530

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -2,13 +2,13 @@
 
 <body>
    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a href="index.php" class="navbar-brand">ShopShop</a>
+      <a href="../index.php" class="navbar-brand">ShopShop</a>
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
       <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
          <div class="navbar-nav">
-            <a href="index.php" class="nav-item nav-link">Home</a>
+            <a href="../index.php" class="nav-item nav-link">Home</a>
             <a href="#" class="nav-item nav-link active">About</a>
          </div>
          <div class="navbar-nav ml-auto">
