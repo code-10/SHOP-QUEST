82a591cab0fec7dbcee31446e709fdc72cc7bb35
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 15:14:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
-<!--HTML boiler plate-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ShopQuest</title>
-  
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    
-    <!--font awesome-->  
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
-    <!--bootstrap js-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    
-</head>
+<?php include_once 'header.php'; ?>
+
 <body>
      <style media="screen">
             .figure {display: table;margin-right: auto;margin-left: auto;}
@@ -145,4 +128,4 @@
   
 </body>
 
-<?php include 'footer.php'; ?>
+<?php include_once 'footer.php'; ?>
