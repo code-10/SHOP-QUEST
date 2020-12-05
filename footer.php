f75772bb9e49d1d8f3855d4d1f246fe6e5b1c723
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 15:06:47 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+ <!--footer-->
+    <!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://doc-shop-shop.herokuapp.com">doc-shop-shop.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+    
+    
+</html>
