72cef3b42b0e47adc2719874d47e2e21e687a5ae
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 17:10:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 			<div class="collapse navbar-collapse" id="navbarCollapse">
 				<div class="navbar-nav">
 					<a href="#" class="nav-item nav-link active">Home</a>
-					<a href="about.php" class="nav-item nav-link">About</a>
+					<a href="pages/about.php" class="nav-item nav-link">About</a>
 				</div>
 				<div class="navbar-nav ml-auto">
 					<?php if(isset($_SESSION['user_name'])) {
