91adecb6f7acf7ddf0fc9d2afc70b964c96b0b10
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 17:42:15 +0530

    Create login_details.php

---
+++ login/login_details.php
@@ -0,0 +1,55 @@
+<?php
+
+session_start();
+
+include '../libraries/chocolates.php';
+
+function check_passwordu($user_name, $password)
+{
+    
+    $con = getCon();
+    
+    $user = $con->query("select * from user where user_name='$user_name';");
+    $res  = $user->fetch_assoc();
+    
+    echo var_dump($res) . "<br>";
+    
+    $password_hash = $res['password'];
+    
+    if (password_verify($password, $password_hash)) {
+        echo "password verified<br>";
+        return True;
+    } else {
+        echo "password not verified<br>";
+        return False;
+    }
+}
+
+
+
+if (isset($_POST['login_user'])) {
+    $user_name = $_POST['user_name'];
+    $password  = $_POST['password'];
+    
+    if (rowExists('user', 'user_name', $user_name)) {
+        if (check_passwordu($user_name, $password)) {
+            //echo "Yes";
+            
+            $_SESSION['user_name'] = $user_name;
+            header("Location:../index.php");
+            die();
+        } else {
+            $wrongpassword = true;
+            header("Location:login.php?wrongpassword=" . $wrongpassword);
+            echo "no2 [password wrong]";
+        }
+    } else {
+        //echo "no1 [user doesn't exist]";
+        header("Location:../register/register.php");
+        die();
+    }
+}
+
+
+
+?>
