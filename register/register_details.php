b17e6c3f0c6b1a223e5d9a6cb440b3ca340d6030
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-05 17:32:03 +0530

    Create register_details.php

---
+++ register/register_details.php
@@ -0,0 +1,52 @@
+<?php include '../libraries/chocolates.php';
+
+if (isset($_POST['register_user']))
+{
+    $con = getCon();
+
+    $u = $_POST['user_name'];
+    $e = $_POST['email'];
+    $p = $_POST['password'];
+
+    
+    if (rowExists('commonpasswords', 'possible_password', $p) || $u == $p)
+    {
+        $commonpassword = true;
+        header("Location:register.php?commonpassword=" . $commonpassword . "user_name=" . $u . "email=" . $e);
+        die();
+    }
+    
+    $p = password_hash($p, PASSWORD_DEFAULT);
+
+    if (rowExists('user', 'user_name', $u))
+    {
+        $userexists = true;
+        header("Location:register.php?userexists=" . $userexists);
+        die();
+
+    }
+    else if (rowExists('user', 'email', $e))
+    {
+        $emailexists = true;
+        header("Location:register.php?emailexists=" . $emailexists);
+        die();
+
+    }
+    else
+    {
+        if (($con->query("insert into user(user_name,email,password) values('$u','$e','$p');")) === True)
+        {
+            //echo "YES";
+            header("Location:../login/login.php");
+            die();
+        }
+        else
+        {
+            $error = true;
+            header("Location:register.php?emailexists=" . $error);
+            die();
+        }
+    }
+}
+
+?>
