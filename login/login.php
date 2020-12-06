8d90e9f84615c39f5541cb76325254e352223dbc
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-06 19:09:12 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,5 +1,5 @@
 <?php include_once '../header.php'; ?>
-<?php $visit=$_GET['visit'];  //echo $visit; ?>
+<?php $visit=$_GET['visit'];  echo $visit; ?>
 <?php $visit_it = $_SESSION['visit_it']; echo $visit_it; ?>
 
 <body>
