ee7cfa6ca739102e303712a7d3b94ad177f72786
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-06 23:44:09 +0530

    Update product_description.php

--- products/product_description.php
+++ products/product_description.php
@@ -139,9 +139,9 @@
 			
 		    <!--start rating-->
 		    <?php for($i=0;$i<$product_rating_star-1;$i++) { ?>
-			<i class="fa fa-star"></i>
+			<i class="fa fa-star" style="background-color:yellow"></i>
 		    <? } ?>
-			<i class="fa fa-star-half-alt"></i>
+			<i class="fa fa-star-half-full"></i>
 		    <?php for($j=0;$j<5-$product_rating_star;$j++) { ?>
 			<i class="fa fa-star"></i>
 		    <? } ?>
