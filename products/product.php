f31b327951f4f21059759df442f295da3e3e6b86
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-07 16:58:08 +0530

    Update product.php

--- products/product.php
+++ products/product.php
@@ -62,6 +62,40 @@
   
 	<br><br>
   
+	
+	<script>
+function showUser(str) {
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET","filter.php?q="+str,true);
+    xmlhttp.send();
+  }
+}
+</script>
+	<form>
+<select name="users" onchange="showUser(this.value)">
+  <option value="">Select a person:</option>
+  <option value="1">Peter Griffin</option>
+  <option value="2">Lois Griffin</option>
+  <option value="3">Joseph Swanson</option>
+  <option value="4">Glenn Quagmire</option>
+  </select>
+</form>
+<br>
+<div id="txtHint"><b>Person info will be listed here...</b></div>
+	
+	
+	<br><br>
+	
+	
  <form method="GET" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
      <div class="container">
   <div class="row text-center">
