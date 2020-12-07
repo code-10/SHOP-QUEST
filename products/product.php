d01a2624881c333688a16840392c6ee2ecbf5c4b
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-07 18:54:30 +0530

    Update product.php

--- products/product.php
+++ products/product.php
@@ -11,6 +11,8 @@
     $sub_cat_name = $_GET['sub_cat_name']; 
     $id_s=$_GET['id_s'];
 
+    $q=$_GET['id_s'];
+
     $visit = $_SERVER['REQUEST_URI'];
   	$visit = substr($visit,1);
 
@@ -80,7 +82,7 @@ function showUser(str) {
         document.getElementById("txtHint").innerHTML = this.responseText;
       }
     };
-    xmlhttp.open("GET","filter.php?q="+str,true);
+    xmlhttp.open("GET","<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?q="+str,true);
     xmlhttp.send();
   }
 }
@@ -97,7 +99,8 @@ function showUser(str) {
 	
 	
 <div id="txtHint"></div>
-	
+
+
   
  <br><br>	
 	
@@ -109,13 +112,27 @@ function showUser(str) {
     $id_s=$_GET['id_s'];
     
   ?>
+	
+	
+	
    <?php
 	
-	$con=getCon();
-	 $res = $con->query("select products.product_id,product_name,min(price) as price,rating from products inner join unique_product on products.product_id=unique_product.product_id where sub_cat_id = '$sub_cat_id' group by products.product_id");	
-	
-	
-	 $product_id=Array();
+	$id_s = intval($_GET['q']);
+
+include_once '../libraries/chocolates.php';
+$con = getCon();
+    if($id_s==1)
+      $res = $con->query("select products.product_id,product_name,min(price) as price,rating from products inner join unique_product on products.product_id=unique_product.product_id where sub_cat_id = '$sub_cat_id' group by products.product_id");
+    else if($id_s==2)
+        $res = $con->query("select products.product_id,product_name,min(price) as price,rating from products inner join unique_product on products.product_id=unique_product.product_id where sub_cat_id = '$sub_cat_id' group by products.product_id order by price");
+    else if($id_s==3)
+      $res = $con->query("select products.product_id,product_name,min(price) as price,rating from products inner join unique_product on products.product_id=unique_product.product_id where sub_cat_id = '$sub_cat_id' group by products.product_id order by price desc");
+    else
+      $res = $con->query("select products.product_id,product_name,min(price) as price,rating from products inner join unique_product on products.product_id=unique_product.product_id where sub_cat_id = '$sub_cat_id' group by products.product_id order by rating desc");
+  
+    
+    
+    $product_id=Array();
     $product_name=Array();
     $product_price=Array();
     $product_rating=Array();
@@ -129,46 +146,46 @@ function showUser(str) {
     }
    
     $n=count($product_id);
+    $c=1;
+$lim=$n/4+1; 
     
+    echo '
+        <p class="display-4 text-center"><?=$cat_name;?></p>
+    <br>
+    ';
+
+    for($j=1;$j<=$lim;$j++){
+    echo '<div class="container">
+  <div class="row p-2">';
+   for($i=1;$i<=4;$i++) { 
+    if(4*($j-1)+$i>$n) break; 
+   echo '<div class="col-sm-6 col-lg-3 text-center">
+      <figure class="figure">
+        <a href="../products/product_description.php?product_id='.$product_id[$c-1].'&&product_name='.$product_name[$c-1].'">
+          <img src="..." class="figure-img img-fluid rounded mx-auto d-block" alt="product" onerror="this.src=\'../assets/black.png\';">
+        </a>
+        <figcaption class="text-center">
+            <h5>'.$product_name[$c-1].'</h5>
+          <h5>Rating : '.$product_rating[$c-1].'&nbsp;&nbsp;</h5>
+           <h5>Price : '.$product_price[$c-1].'&nbsp;&nbsp;</h5> 
+           </figcaption>
+      </figure>
+    </div>';
+       $c++;}
+        
+       echo '
+      </div> 
+     </div>';
+    }
+
+	
+	
 	
 	?>
 
-	<!--code from index.php card decks logic added-->
-   <p class="display-4 text-center"><?=$cat_name;?></p>
-    <br>
-    <?$c=1; $lim=$n/4+1; for($j=1;$j<=$lim;$j++){ ?>
-    <div class="container">
-  <div class="row p-2">
-    <? for($i=1;$i<=4;$i++){ ?> 
-    <? if(4*($j-1)+$i>$n) break; ?>
-   <div class="col-sm-6 col-lg-3 text-center">
-      <figure class="figure">
-        <a href='../products/product_description.php?product_id=<?=$product_id[$c-1]?>&&product_name=<?=$product_name[$c-1]?>'>
-          <img src="../assets/<?=$product_id[$c-1]?>.jpeg" class="figure-img img-fluid rounded mx-auto d-block" alt="product" onerror="this.src='../assets/black.png';">
-        </a>
-        <figcaption class="text-center">
-            <h5><?=$product_name[$c-1]?></h5>
-          <h5>Rating : <?=$product_rating[$c-1]?>&nbsp;&nbsp;</h5>
-           <h5>Price : <?=$product_price[$c-1]?>&nbsp;&nbsp;</h5> 
-           </figcaption>
-      </figure>
-    </div>
-  <? $c++;} ?>
-      </div> 
-     </div>
-    <? } ?>  
+	
 	
  
-    
-    
-    
-    
-   
-    
-    
-    
-    
-    
     
     
     
