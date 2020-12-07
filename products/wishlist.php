6660098d65350bf20ee133923d4a6d483e0c0a39
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-07 21:52:46 +0530

    Update wishlist.php

--- products/wishlist.php
+++ products/wishlist.php
@@ -4,23 +4,36 @@
   include '../libraries/chocolates.php';
   $con = getCon();  
 
+  $wishdo=$_GET['wishdo'];
   $product_id=$_GET['product_id'];
   $user=$_SESSION['user_name'];
   $product_name=$_GET['product_name'];
   
+
+  
   if(isset($_SESSION['user_name']))
   {
+    if($wishdo=="yes"){
       if(($con->query("insert into wishlist(user_name,product_id) values('$user','$product_id');"))===True)
       {
         $wstate=True;
         header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&wstate=".$wstate);
         die();
       }
-      else
-      {
-        $astate=True;
-        header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&astate=".$astate);
-        die();
+     }
+    
+    
+    else if($wishdo=="no")
+    {
+         $sql="delete from wishlist where user_name='$user' and product_id='$product_id'" ;
+   
+           if($con->query($sql)===True)
+           {    
+                  $wstate=False;
+                  header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&wstate=".$wstate);
+                  die();
+           }
+  
       }
   }
 else
