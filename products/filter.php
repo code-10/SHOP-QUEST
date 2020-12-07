c3af368b6b55cee278c4d0e04751841c7618461f
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-07 17:46:09 +0530

    Update filter.php

--- products/filter.php
+++ products/filter.php
@@ -34,7 +34,7 @@ $lim=$n/4+1;
     
     echo '
       <!--code from index.php card decks logic added-->
-   <p class="display-4 text-center"><?=$cat_name;?></p>
+   <p class="display-4 text-center">'.$cat_name.'</p>
     <br>
      '.for($j=1;$j<=$lim;$j++){.'
     <div class="container">
