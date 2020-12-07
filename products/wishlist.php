fe8918cac27526f55bb005883ea76e0589b54696
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-07 21:31:38 +0530

    Create wishlist.php

---
+++ products/wishlist.php
@@ -0,0 +1,33 @@
+<?php
+  
+  session_start();
+  include '../libraries/chocolates.php';
+  $con = getCon();  
+
+  $product_id=$_GET['product_id'];
+  $user=$_SESSION['user_name'];
+  $product_name=$_GET['product_name'];
+  
+  if(isset($_SESSION['user_name']))
+  {
+      if(($con->query("insert into wishlist(user_name,product_id) values('$user','$product_id');"))===True)
+      {
+        $wstate=True;
+        header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&wstate=".$wstate);
+        die();
+      }
+      else
+      {
+        $astate=True;
+        header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&astate=".$astate);
+        die();
+      }
+  }
+else
+{
+  $nolog=true;
+  header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&nolog=".$nolog);
+        die();
+}
+    
+?>
