cd4700b81572330b71a597f2e5d88ef7e1092340
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-08 08:44:10 -0800

    Update index.php

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
 				<div class="col-lg-6 col-xs-6 col-sm-6 col-md-6">
 					<div class="card text-center">
 						<a href='sub_categories/sub_category.php?cat_id=<?=$c;?>&&cat_name=<?=$categories[$c+1-1];?>' class="stretched-link">
-  						<img class="card-img-top" src="assets/categories/cat<?=$i+1?>.png" alt="Card image cap" style="width:16%;height:16%;">
+  						<img class="card-img-top" src="assets/categories/cat<?=$i+1?>.png" alt="Category" style="width:16%;height:16%;" onerror="this.src='assets/black.png';">
   							<div class="card-body">
     								<h5 class="card-title"><?=$categories[$i];?></h5>
   							</div>
@@ -121,7 +121,7 @@
 				<div class="col-lg-6 col-xs-6 col-sm-6 col-md-6">
 					<div class="card text-center">
 						<a href='sub_categories/sub_category.php?cat_id=<?=$c+1;?>&&cat_name=<?=$categories[$c-1];?>' class="stretched-link">
-  						<img class="card-img-top" src="assets/categories/cat<?=$i+2?>.png" alt="Card image cap" style="width:16%;height:16%;">
+  						<img class="card-img-top" src="assets/categories/cat<?=$i+2?>.png" alt="Category" style="width:16%;height:16%;" onerror="this.src='assets/black.png';">
   							<div class="card-body">
     								<h5 class="card-title"><?=$categories[$i+1];?></h5>
   							</div>
