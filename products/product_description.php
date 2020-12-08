5fb2c93c0644ead328d17cf7f0362d42e9466812
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-08 06:29:32 -0800

    Update product_description.php

--- products/product_description.php
+++ products/product_description.php
@@ -206,8 +206,8 @@
 		<!--buy and add to cart-->
 		    <div class="row">
 			    <div class="col-3"></div>
-			    <div class="col-5 text-right">
-				    Quantity : 
+			    <div class="col-6 text-left">
+				    Quantity
 			    	    <div class="form-group">
     						<select class="form-control" id="qty" name="quantity">
       							<option value="1" selected>1</option>
@@ -217,7 +217,7 @@
     						</select>
   				    </div>
 			    </div>
-			    <div class="col-4"></div>
+			    <div class="col-3"></div>
 		    </div>
 		    <div class="row">
 			    <div class="col-3"></div>
