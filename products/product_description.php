cb55372d8a97e01d746eb1af0cf848d6c515e150
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-08 02:37:25 -0800

    Update product_description.php

--- products/product_description.php
+++ products/product_description.php
@@ -86,10 +86,7 @@
     
     	$n=count($product_description);
 	
-	
-
-    $wstate=$_SESSION['wstate'];
-     /*$wstate=$_GET['wstate'];*/                                     
+	                              
      $nolog=$_GET['nolog'];
      $nostock=$_GET['nostock'];
      $cartd=$_GET['cartd'];
@@ -99,6 +96,12 @@
      {
 	$nolog=False;     
      }
+	
+	
+     //to check if wishlisted
+	$user=$_SESSION['user_name'];
+	$res=$con->query("select user_name from wishlist where product_id='$product_id' and user_name='$user'");
+
   
   ?>
   
@@ -133,12 +136,12 @@
 				
                 	<div class="card-body p-1 m-1">
                     <h5 class="card-title text-center mt-2" style="font-size:40px;"><?=$product_name;?>
-			    <?php if(sset($_SESSION['$product_id'])) 
+			    <?php if($res->num_rows>0) 
 					echo '<a class="ml-2" href="wishlist.php?product_id='.$product_id.'&&product_name='.$product_name.'&&wishdo=no"><i class="fa fa-heart" style="color:#ff008a"></i></a>';
 			    	else
 					echo '<a class="ml-2" href="wishlist.php?product_id='.$product_id.'&&product_name='.$product_name.'&&wishdo=yes"> <i class="fa fa-heart-o" style="color:#ff008a"></i></a>';
 			    if(isset($_SESSION['user_name'])){
-			    	if(isset($_SESSION['$product_id']))
+			    	if($res->num_rows>0)
           				echo "<h4 class='animate__animated animate__fadeOut' style='--animate-duration: 4s;'>Wishlisted</h4>";
       			    	else
           				echo "<h4 class='animate__animated animate__fadeOut' style='--animate-duration: 4s;'>Removed from Wishlist</h4>";
