b46803e90c96a71497dae035d6cbb04e906dbde5
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-09 16:37:50 +0530

    Update cart.php

--- products/cart.php
+++ products/cart.php
@@ -1,1 +1,23 @@
-<?php?>
+<?php
+
+      include_once '../header.php';
+      include_once '../libraries/chocolates.php';
+      session_start();
+      
+      $user=$_SESSION['user_name'];
+      $show=$_GET['show'];
+      $unique_type_id=$_GET['unique_type_id'];
+      $product_id=$_GET['product_id'];
+      $product_name=$_GET['product_name'];
+
+      if(!(isset($_SESSION['user_name'])))
+      {
+            $nolog=true;
+            header("Location:product_description.php?product_id=".$product_id."&&product_name=".$product_name."&&nolog=".$nolog."&&show=".$show);
+            die(); 
+      }
+      
+?>
+
+
+<?php include_once '../footer.php'; ?>
