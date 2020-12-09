91d390a16315a02d2748fc3d5a6fa57a7679b763
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-09 00:32:08 -0800

    Update product.php

--- products/product.php
+++ products/product.php
@@ -37,7 +37,7 @@
                     echo '<a href="../login/logout.php" class="nav-item nav-link">Logout</a>';
                 }
                 else{
-                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';';
+                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
                 }
             ?>
         </div>
