517fc1a59faebfe39b73d6b51f95bbb08459c3b2
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-09 13:02:01 +0530

    Create login_details.php

---
+++ sign_in/login_details.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+include '../libraries/chocolates.php';
+
+$visit=$_SESSION['visit'];
+
+function check_passwordu($user_name, $password)
+{
+    
+    $con = getCon();
+    
+    $user = $con->query("select * from user where user_name='$user_name';");
+    $res  = $user->fetch_assoc();
+    
+    echo var_dump($res) . "<br>";
+    
+    $password_hash = $res['password'];
+    
+    if (password_verify($password, $password_hash)) {
+        echo "password verified<br>";
+        return True;
+    } else {
+        echo "password not verified<br>";
+        return False;
+    }
+}
+
+
+function check_passworde($email, $password)
+{
+    
+    $con = getCon();
+    
+    $user = $con->query("select * from user where email='$email';");
+    $res  = $user->fetch_assoc();
+    
+    echo var_dump($res) . "<br>";
+    
+    $password_hash = $res['password'];
+    
+    if (password_verify($password, $password_hash)) {
+        echo "password verified<br>";
+        return True;
+    } else {
+        echo "password not verified<br>";
+        return False;
+    }
+}
+
+
+
+
+
+
+
+
+
+if (isset($_POST['login_user'])) {
+    $user_name = $_POST['user_name'];
+    $email = $_POST['username'];
+    //$user_name = str_replace(' ', '', $user_name);
+    $user_name = strtolower($user_name);
+    $password  = $_POST['password'];
+    
+    if (rowExists('user', 'user_name', $user_name)||rowExists('user','email',$email)) {
+        if (check_passwordu($user_name, $password)||check_passworde($email,$password)) {
+            //echo "Yes";
+            
+            $_SESSION['user_name'] = $user_name;
+            header("Location:../".$visit);
+            die();
+        } else {
+            $wrongpassword = true;
+            header("Location:login.php?wrongpassword=" . $wrongpassword."&&visit=".$visit);
+            echo "no2 [password wrong]";
+        }
+    } else {
+        //echo "no1 [user doesn't exist]";
+        header("Location:../register/register.php?".$visit."&&user=no");
+        die();
+    }
+}
+
+
+
+?>
