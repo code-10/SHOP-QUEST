66cde3f2c8097e825636fff8d3f28ef115c7bedc
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-09 00:40:32 -0800

    Update sub_category.php

--- sub_categories/sub_category.php
+++ sub_categories/sub_category.php
@@ -32,7 +32,7 @@
             <?php if(isset($_SESSION['user_name'])) {
                     echo '<a href="../pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
                     echo '<a href="../product/cart_display.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
-                    echo '<a href="../login/logout.php" class="nav-item nav-link">Logout</a>';
+                    echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
                 }
                 else{
                     echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
