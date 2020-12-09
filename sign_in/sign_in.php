154cd96b59ab4c4008f3ce059a82793d1b51d072
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-09 11:14:55 +0530

    Update sign_in.php

--- sign_in/sign_in.php
+++ sign_in/sign_in.php
@@ -48,7 +48,7 @@
   
 
 <div class="container">
-    	<div class="row">
+    	<div class="row d-flex justify-content-center">
 			<div class="col-md-6 col-md-3">
 				<div class="panel panel-login">
 					<div class="panel-heading">
