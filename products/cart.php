5682405a1d69fafe77d929d1db44394a03ae95f6
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-10 15:32:50 +0530

    Update cart.php

--- products/cart.php
+++ products/cart.php
@@ -90,11 +90,11 @@
 		</div>
 		<div class="col-lg-4 col-sm-4 col-xs-4 col-md-4 col-8">
 			<div class="row">
-				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><?=$product_name[$i]?></p></div>
-				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;">color : <?=$product_color[$i]?></p></div>
-				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;">size : <?=$product_size[$i]?></p></div>
-				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;">quantity : <?=$product_qty[$i]?></p></div>
-				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;">price : <?=$product_qty[$i]*$product_price[$i]?></p></div>
+				<div class="col-lg-4 col-sm-4 col-xs-4 col-md-4 col-8"><p style="margin-bottom:0px;"><?=$product_name[$i]?></p></div>
+				<div class="col-lg-4 col-sm-4 col-xs-4 col-md-4 col-8"><p style="margin-bottom:0px;">color : <?=$product_color[$i]?></p></div>
+				<div class="col-lg-4 col-sm-4 col-xs-4 col-md-4 col-8"><p style="margin-bottom:0px;">size : <?=$product_size[$i]?></p></div>
+				<div class="col-lg-4 col-sm-4 col-xs-4 col-md-4 col-8"><p style="margin-bottom:0px;">quantity : <?=$product_qty[$i]?></p></div>
+				<div class="col-lg-4 col-sm-4 col-xs-4 col-md-4 col-8"><p style="margin-bottom:0px;">price : <?=$product_qty[$i]*$product_price[$i]?></p></div>
 			</div>
 		</div>
 	</div>
