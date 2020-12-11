d4c23f84619078f5801cfa5ecdeb01c8721d8e07
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-11 18:30:08 +0530

    Update delivery_and_payment.php

--- products/delivery_and_payment.php
+++ products/delivery_and_payment.php
@@ -88,14 +88,14 @@
 
 <?php for($i=0;$i<$n;$i++) { ?>	
 	<div class="row m-4 d-flex justify-content-center">
-		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-6"></div>
+		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-12"></div>
 		<div class="col-lg-10 col-sm-10 col-xs-10 col-md-10 col-6">
 			<div class="row">
 				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><?=$product_name[$i]?></p></div>
 				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><?=$product_color[$i]?></p></div>
 				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><?=$product_size[$i]?></p></div>
 				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><p><?=$product_qty[$i]?></p></div>
-				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><i class="fa fa-rupee"></i> <?=$product_qty[$i]*$product_price[$i]?></p></div>
+				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"><p style="margin-bottom:0px;"><i class="fa fa-rupee"></i> <?=$total_price?></p></div>
 				<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-8"></div>
 			</div>
 		</div>
@@ -105,7 +105,7 @@
 
 	
 <div class="row m-4 d-flex justify-content-center">
-	<div class="col-lg-6 col-sm-6 col-xs-6 col-md-6 col-6">
+	<div class="col-lg-6 col-sm-6 col-xs-6 col-md-6 col-12">
 		<form method="POST" action="delivery_and_payment.php">
   			<div class="form-group">
     				<label for="exampleFormControlTextarea1">Enter your Address</label>
@@ -116,7 +116,7 @@
 	<div class="col-lg-6 col-sm-6 col-xs-6 col-md-6 col-6">
 		<form method="POST" action="delivery_and_payment.php">
   			<div class="form-group">
-    				<label for="exampleFormControlTextarea1">Enter your Address</label>
+    				<label for="exampleFormControlTextarea1">Enter total price</label>
 				<input type="number" class="form-control" placeholder="Just enter the total price">
   			</div>
 		</form>
