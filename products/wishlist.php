ee4a289f4bd8fc2055d173571aef1d2536fc55ce
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-11 07:59:03 -0800

    Update wishlist.php

--- products/wishlist.php
+++ products/wishlist.php
@@ -44,3 +44,48 @@ else
 }
     
 ?>
+
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="../index.php" class="navbar-brand">ShopQuest</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+            <a href="../pages/about.php" class="nav-item nav-link">About</a>
+        </div>
+         <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['user_name'])) {
+                    echo '<a href="../pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
+                    echo '<a href="../products/cart.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
+                    echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
+                }
+                else{
+                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
+                }
+            ?>
+        </div>
+    </div>
+</nav>
+  
+  
+  				<!--search bar-->
+					<div id="search" class="mb-2" style="background-color:black;">
+						<div class="text-center">
+						<form method="GET" action="../pages/search.php" class="form-inline input-group d-flex justify-content-center" style="padding:0.60rem!important">
+      							<div class="input-group">
+  								<input type="text" class="form-control" name="search_product" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2" required>
+  								<div class="input-group-append">
+    									<button class="btn btn-outline-light" type="submit">Search</button>
+  								</div>
+							</div>
+    						</form>
+						</div>
+					</div>
+
+
+
