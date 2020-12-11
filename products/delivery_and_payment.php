9d52c5358b0b0019e23a3f83dc699ae60bf2b461
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-11 19:04:00 +0530

    Update delivery_and_payment.php

--- products/delivery_and_payment.php
+++ products/delivery_and_payment.php
@@ -93,7 +93,7 @@
 	
 
 <?php for($i=0;$i<$n;$i++) { ?>	
-	<div class="row m-4 d-flex justify-content-center">
+	<div class="row m-4 d-flex justify-content-left">
 		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-12"></div>
 		<div class="col-lg-10 col-sm-10 col-xs-10 col-md-10 col-6">
 			<div class="row">
@@ -109,7 +109,7 @@
 
 <?php } ?>	
 
-<div class="row m-4 d-flex justify-content-left">
+<div class="row m-4 d-flex justify-content-center">
 		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2 col-6">
 		</div>
 		<div class="col-lg-10 col-sm-10 col-xs-10 col-md-10 col-6">
