3374157ea5ffe87d017d4a9fabf9ab5c0b4edf67
SSSShivani <42977733+SSSShivani@users.noreply.github.com>
2020-12-13 17:47:33 +0530

    Update rateandreview.php

--- products/rateandreview.php
+++ products/rateandreview.php
@@ -30,7 +30,26 @@
     if(isset($_POST['submit_rating_and_review']))
     {
      	$con->query("update order_contents set o_rating='$rating',review='$review' where unique_type_id='$unique_type_id' and order_id='$order_id_rate'");   
-	   
+	    
+	$rar=$con->query("select p.rating_no,p.product_id,p.rating_sum from products as p,unique_product as up where up.product_id=p.product_id and up.unique_type_id='$unique_type_id'");
+	
+	$rno=Array();
+	$rsum=Array();
+	$product_id_rar=Array();
+	    
+	while($rard=$rar->fetch_assoc())
+	{
+		$rno[]=$rard['rating_no'];
+		$rsum[]=$rard['rating_sum'];
+		$product_id_rar[]=$rard['product_id'];
+	}
+	 
+	$rno=$rno[0]+1;
+	$rsum=$rsum[0]+$rating;
+	$final_rating=$rsum/$rno;
+	    
+	$con->query("update products set rating_no='$rno',rating_sum='$rsum',rating='$final_rating' where product_id='$product_id_rar[0]'");
+	    
     }
     
   
