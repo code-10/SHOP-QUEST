a5ab4cb8a538d6d931d91715fb5111f3c21f352a
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-13 12:32:45 +0530

    Update rateandreview.php

--- products/rateandreview.php
+++ products/rateandreview.php
@@ -16,12 +16,13 @@
         die(); 
     } 
     
-    
+	echo $order_id_rate;
+        echo "<br>";
+        echo $unique_type_id;    
+
     if(isset($_POST['submit_rating']))
     {
-        echo $order_id_rate;
-        echo "<br>";
-        echo $unique_type_id;
+        
     }
     
   
