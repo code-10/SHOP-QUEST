ff07b778506a929d53e5d483d3c0e0cf872d3a0c
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-13 13:07:05 +0530

    Update successful.php

--- products/successful.php
+++ products/successful.php
@@ -162,12 +162,18 @@
 						<div class="collapse m-2" id="collapse<?=$j?>">
   							<div class="card card-body">
 								<form method="POST" action="rateandreview.php" class="form-inline input-group d-flex justify-content-center">
-								<div class="input-group mb-3">
+								<div class="form-group mb-3">
 									<input type="hidden" name="unique_type_id" value="<?=$unique_type_id[$j]?>" />
 									<input type="hidden" name="order_id_rate" value="<?=$order_id_detail?>" />
-  									<input type="number" class="form-control" name="rating" placeholder="Rating" aria-label="Rating" aria-describedby="basic-addon2" required>	
-    									<button class="btn btn-dark" name="submit_rating" type="submit">Submit rating</button>
+									<label for="rating">Rating</label>
+  									<input type="number" class="form-control" name="rating" placeholder="Rating" aria-label="Rating" aria-describedby="basic-addon2" required>
 								</div>
+								<div class="form-group mb-3">
+    									<label for="review">Review</label>
+    									<textarea class="form-control" id="review" rows="3" name="review"></textarea>
+  								</div>
+									
+								<button class="btn btn-dark" name="submit_rating_and_review" type="submit">Submit rating</button>
 								</form>
   							</div>
 						</div>
