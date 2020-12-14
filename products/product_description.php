656b37bfb1b5d2087c00080c1d4086cdf24b89a2
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-14 14:52:23 +0530

    Update product_description.php

--- products/product_description.php
+++ products/product_description.php
@@ -213,7 +213,7 @@
 		    <div class="row">
 			    <div class="col-3"></div>
 			    <div class="col-6 text-left">
-				    <?php if($remaining_quantity==0) { ?>
+				    <?php if($remaining_quantity<=0) { ?>
 					<h4 class="card-text ml-4 mr-4 text-center">OUT OF STOCK</h4>
 				    	<h4 class="card-text ml-4 mr-4 text-center">Choose other Options</h4>
 			    	    <?php } else { ?>
