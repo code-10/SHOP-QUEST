216a5243bfa7d471d23a8acd19bd5092dae18334
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-14 23:05:13 +0530

    Update rateandreview.php

--- products/rateandreview.php
+++ products/rateandreview.php
@@ -50,6 +50,10 @@
 	    
 	$con->query("update products set rating_no='$rno',rating_sum='$rsum',rating='$final_rating' where product_id='$product_id_rar[0]'");
 	    
+	 header("Location:successful.php?order_details=yes&&order_id_detail='$order_id_rate'");
+        die();    
+	   
+	    
     }
     
   
