b9771c9b49257b2500d13a2d1fc610f3bcdca289
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-14 12:56:59 +0530

    Update seller_enter.php

--- seller/seller_enter.php
+++ seller/seller_enter.php
@@ -1,1 +1,17 @@
-<?php?>
+<?php include_once '../header.php'; ?>
+<?php include_once '../libraries/chocolates.php'; ?>
+
+<?php 
+      
+      session_start(); 
+
+      if(!(isset($_SESSION['user_name'])))
+      	{
+            header("Location:../index.php");
+            die(); 
+      	} 
+
+?>
+
+
+<?php include_once '../footer.php'; ?>
