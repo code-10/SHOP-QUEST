74a5b850ac05095a955994243678bbb2da2728ec
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-15 17:41:17 +0530

    Update admin_enter.php

--- admin/admin_enter.php
+++ admin/admin_enter.php
@@ -1,1 +1,61 @@
-<?php?>
+<?php include_once '../header.php'; ?>
+<?php include_once '../libraries/chocolates.php'; ?>
+
+<?php 
+      
+      session_start(); 
+
+      if(!(isset($_SESSION['user_name']))||($_SESSION['user_name']!="root"))
+      	{
+            header("Location:../index.php");
+            die(); 
+      	} 
+
+
+		
+?>
+
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="../index.php" class="navbar-brand">ShopShop</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+            <a href="../pages/about.php" class="nav-item nav-link">About</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['user_name'])){
+                    echo '<a href="../pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o"> '.$_SESSION['user_name'].'</i></a>';
+                    echo '<a href="../products/cart.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
+                    echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
+                }
+                else{
+                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
+                }
+            ?>
+        </div>
+    </div>
+</nav>
+
+
+      
+  
+
+      
+      
+  
+</body>
+      
+      
+
+
+
+
+
+
+<?php include_once '../footer.php'; ?>
