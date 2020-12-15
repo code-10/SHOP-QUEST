5209825916cb49e0671e6ba04f33ccdb46ea88c2
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-15 17:53:32 +0530

    Update check_db.php

--- admin/check_db.php
+++ admin/check_db.php
@@ -1,1 +1,167 @@
-<?php?>
+<?php
+        include_once '../header.php';
+        session_start();
+        include '../libraries/chocolates.php';
+        $category=Array();
+        $con = getCon();
+        $res = $con->query("select * from categories");
+        while($ele = $res->fetch_assoc())
+            $category[]=$ele;
+        
+       
+        
+
+
+        $sub_category=Array();
+        $res = $con->query("select * from sub_categories");
+        while($ele = $res->fetch_assoc())
+            $sub_category[]=$ele;
+
+
+
+        
+        $products=Array();
+        $res = $con->query("select * from products");
+        while($ele = $res->fetch_assoc())
+            $products[]=$ele;
+
+
+
+
+        $uniq_prod=Array();
+        $res = $con->query("select * from unique_product");
+        while($ele = $res->fetch_assoc())
+            $uniq_prod[]=$ele;
+        
+        
+?>
+
+<style>
+    .bs-example{
+        margin: 20px;
+    }
+    .accordion .fa{
+        margin-right: 0.5rem;
+    }
+</style>
+<script>
+    $(document).ready(function(){
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+        	$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        }).on('hide.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+        });
+    });
+</script>
+
+
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="../index.php" class="navbar-brand">ShopShop</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+            <a href="../pages/about.php" class="nav-item nav-link">About</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['user_name'])){
+                    echo '<a href="../pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o"> '.$_SESSION['user_name'].'</i></a>';
+                    echo '<a href="../products/cart.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
+                    echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
+                }
+                else{
+                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
+                }
+            ?>
+        </div>
+    </div>
+</nav>
+
+     
+   
+        
+        
+        
+<div class="bs-example">
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"><i class="fa fa-plus"></i>categories</button>									
+                </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                     <? foreach($category as $cat) { ?>
+                        <p><?=$cat['cat_id']."=>".$cat['cat_name']?></p>
+                        <? } ?>
+                </div>
+            </div>
+        </div>
+            
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo"><i class="fa fa-plus"></i>sub categories</button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                   <? foreach($sub_category as $cat) { ?>
+                        <p><?=$cat['sub_cat_id']."=>".$cat['sub_cat_name']."=>".$cat['cat_id']?></p>
+                        <? } ?>
+                </div>
+            </div>
+        </div>
+            
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                    <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree"><i class="fa fa-plus"></i>Products</button>
+                </h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                   <? foreach($products as $p) { ?>
+                        <p><?=$p['product_id']."=>".$p['product_name']."=>".$p['sub_cat_id']."=>".$p['product_brand']."=>".$p['product_description']."=>".$p['rating']?></p>
+                        <? } ?>
+                </div>
+            </div>
+        </div> 
+            
+        <div class="card">
+            <div class="card-header" id="headingFour">
+                <h2 class="mb-0">
+                    <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour"><i class="fa fa-plus"></i>Unique Products</button>
+                </h2>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                <div class="card-body">
+                   <? foreach($uniq_prod as $p) { ?>
+                        <p><?=$p['unique_type_id']."=>".$p['product_id']."=>".$p['price']."=>".$p['quantity']."=>".$p['seller_user_name']."=>".$p['color']."=>".$p['size']?></p>
+                        <? } ?>
+                </div>
+            </div>
+        </div>        
+            
+        
+    </div>
+</div>
+  
+  
+  
+  
+</body>
+
+<?php include_once '../footer.php'; ?>
