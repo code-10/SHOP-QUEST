a998397e05af49ca668fab1ff61c9744ca1054d0
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-17 01:15:12 +0530

    Update enter_data.php

--- admin/enter_data.php
+++ admin/enter_data.php
@@ -11,6 +11,84 @@
             die(); 
       	} 
 
+      
+        
+        if (isset($_POST['categories_submit'])) {
+                $cat_id = $_POST['cat_id'];
+        $cat_name = $_POST['cat_name'];
+                $con = getCon();
+              if(($con->query("insert into categories(cat_id,cat_name) values('$cat_id','$cat_name')"))===True){
+                //echo "YES";
+                header("Location:admin_display.php");
+                die();
+        }
+        else
+                echo $con->error;  
+        }
+           
+
+        if (isset($_POST['sub_categories_submit'])) {
+                $con = getCon();
+                $sub_cat_id = $_POST['sub_cat_id'];
+        $sub_cat_name = $_POST['sub_cat_name'];
+        $cat_id = $_POST['cat_id'];
+        
+        if(($con->query("insert into sub_categories(sub_cat_id,sub_cat_name,cat_id) values('".mysqli_real_escape_string($con,$sub_cat_id)."','".mysqli_real_escape_string($con,$sub_cat_name)."','".mysqli_real_escape_string($con,$cat_id)."')"))===True){
+                //echo "YES";
+                header("Location:admin_display.php");
+                die();
+        }
+        else
+                echo $con->error;
+        }
+
+
+       
+        
+
+        if (isset($_POST['products'])) {
+                $con = getCon();
+                $product_id = $_POST['product_id'];
+                $product_name = $_POST['product_name'];
+                $sub_cat_id = $_POST['sub_cat_id'];
+                $product_brand = $_POST['product_brand'];
+                $product_description = $_POST['product_description'];
+                $product_rating = $_POST['product_rating'];
+        
+        if(($con->query("insert into products(product_id,product_name,sub_cat_id,product_brand,product_description,rating) values('".mysqli_real_escape_string($con,$product_id)."','".mysqli_real_escape_string($con,$product_name)."','".mysqli_real_escape_string($con,$sub_cat_id)."','".mysqli_real_escape_string($con,$product_brand)."','".mysqli_real_escape_string($con,$product_description)."','".mysqli_real_escape_string($con,$product_rating)."')"))===True){
+                //echo "YES";
+                header("Location:admin_display.php");
+                die();
+        }
+        else
+                echo $con->error;
+        }
+
+
+         if (isset($_POST['uniq_prod'])) {
+                $con = getCon();
+                $product_id = $_POST['uniq_product_id'];
+                $price = $_POST['price'];
+                $quantity = $_POST['quantity'];
+                $seller_user_name = $_POST['seller_user_name'];
+                $color = $_POST['color'];
+                $size = $_POST['size'];
+        
+        if(($con->query("insert into unique_product(product_id,price,quantity,seller_user_name,color,size) values('".mysqli_real_escape_string($con,$product_id)."','".mysqli_real_escape_string($con,$price)."','".mysqli_real_escape_string($con,$quantity)."','".mysqli_real_escape_string($con,$seller_user_name)."','".mysqli_real_escape_string($con,$color)."','".mysqli_real_escape_string($con,$size)."')"))===True){
+                //echo "YES";
+                header("Location:admin_display.php");
+                die();
+        }
+        else
+                echo $con->error;
+        }
+
+
+
+
+
+
+
 ?>
 
 <body>
