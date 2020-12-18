dcea1bd7df04c152ac689cba2eca1019303d2fec
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-18 23:40:41 +0530

    Update enter_data.php

--- admin/enter_data.php
+++ admin/enter_data.php
@@ -84,10 +84,7 @@
                 $cat_id = $_POST['cat_id'];
                 $cat_name = $_POST['cat_name'];
 		
-		echo $cat_id_fill_use;
-		echo $cat_name;
-		
-              if(($con->query("insert into categories(cat_id,cat_name) values('$cat_id','$cat_name')"))===True){
+              if(($con->query("insert into categories(cat_id,cat_name) values('$cat_id_fill_use','$cat_name')"))===True){
                 //echo "YES";
                 //header("Location:admin_enter.php?admin_enter_main=yes");
                 //die();
