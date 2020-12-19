73959234e4fbddecbf2911b4d6718a251c41b2a3
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-19 11:31:11 +0530

    Update sell_request.php

--- admin/sell_request.php
+++ admin/sell_request.php
@@ -247,7 +247,6 @@
      
       
       <?php if($sell_request_main=="yes") { ?>
-      <div class="row">
       <? for($i=0;$i<$n;$i++) { ?>
 <div class="card m-4">
   <div class="card-header">seller name : <?=$seller_name[$i]?></div>
@@ -276,7 +275,6 @@
 </div>
 </div>  
   <? } ?>   
-	</div>
   
   <?php } else if($admin_check_sell=="yes") { ?>
       
