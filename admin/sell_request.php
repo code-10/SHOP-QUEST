837cbebf6de0c007cc3b31dce22a73311a96d549
Manoj Kumar <manojkumar333619@gmail.com>
2020-12-19 10:34:23 +0530

    Update sell_request.php

--- admin/sell_request.php
+++ admin/sell_request.php
@@ -47,7 +47,7 @@
  <?php
       
       //get info from stpore info
-        
+        if($sell_request_main=="yes"){
         $con=getCon();
         $sql="select * from store_info";
     
@@ -84,6 +84,7 @@
   
   $n=count($storeinfoid);
     
+        }
       
       
       
@@ -96,8 +97,7 @@
       
       
       
-      
-      
+      if($admin_check_sell=="yes"){
       
       //check by admin
       $storeinfoid=$_GET['storeinfoid'];
@@ -137,8 +137,59 @@
   
   $n=count($storeinfoid);
       
+      }
       
       
+      
+      
+      
+      
+      if($approve_sell)
+      {
+            $con=getCon();
+
+  
+      $subcategoryid=$_POST['subcatid'];
+      $productid=$_POST['product_id'];
+      $productname=$_POST['product_name'];
+      $productbrand=$_POST['product_brand'];
+      $productdescription=$_POST['product_description'];
+      $price=$_POST['price'];
+      $quantity=$_POST['qty'];
+      $color=$_POST['color'];
+      $size=$_POST['size'];
+      $sellername=$_POST['seller_name'];
+      $rating=$_POST['rating'];          
+
+     
+  
+      //For Store info
+      $approved=$_POST['approve'];
+      $storeinfoid=$_POST['storeinfoid'];
+      $done=1;      
+
+      //for updating store info
+      $sql1="update store_info set approved='$done' where store_info_id='$storeinfoid'";
+      $sql2="insert into products(product_id,product_name,sub_cat_id,product_brand,product_description,rating) values('".mysqli_real_escape_string($con,$productid)."','".mysqli_real_escape_string($con,$productname)."','".mysqli_real_escape_string($con,$subcategoryid)."','".mysqli_real_escape_string($con,$productbrand)."','".mysqli_real_escape_string($con,$productdescription)."','".mysqli_real_escape_string($con,$rating)."')";
+      $sql3="insert into unique_product(product_id,price,quantity,seller_user_name,color,size) values('".mysqli_real_escape_string($con,$productid)."','".mysqli_real_escape_string($con,$price)."','".mysqli_real_escape_string($con,$quantity)."','".mysqli_real_escape_string($con,$sellername)."','".mysqli_real_escape_string($con,$color)."','".mysqli_real_escape_string($con,$size)."')";
+      
+      if($con->query($sql2)===True)
+      {
+        if($con->query($sql3)===True)
+        {
+          if($con->query($sql1)===True)
+          {
+              header("Location:sell_request.php");
+                die();
+          }
+        }
+      }
+      else
+      {
+        echo "something went wrong"; 
+      }  
+            
+      }
       
     
     
@@ -256,11 +307,8 @@
     <button type="submit" name="verify_product" class="btn btn-dark">Approve</button>
     </form>     
       
-      <?php } else if($approve_sell) { ?>
-            
-      <p>approve sell</p>
-      
       <?php } ?>
+            
   
   
   
