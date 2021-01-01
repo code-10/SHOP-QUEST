cbe7a682bcabae5e4bef8e01940a93898add5442
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-01 07:08:55 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,8 +9,9 @@
   <!-- Footer Elements -->
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+  <div class="footer-copyright text-center py-3"><!--© 2020 Copyright:-->
     <a href="https://doc-shop-shop.herokuapp.com">doc-shop-shop.com</a>
+      <p class="m-4"><a href="https://github.com/code-10/SHOP-QUEST"><i class="fa fa-github mr-4" style="color:black;"></i>Shopquest</a></p>
   </div>
   <!-- Copyright -->
 
