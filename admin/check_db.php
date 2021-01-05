66833e4fe48a99cb1e1648677ba4116ae12c84b9
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 15:06:56 +0530

    Update check_db.php

--- admin/check_db.php
+++ admin/check_db.php
@@ -40,30 +40,6 @@
         
 ?>
 
-<?php
-
-	$edit_delete_product_id=$_POST['edit_delete_product_id'];
-	$edit_delete_unique_type_id=$_POST['edit_delete_unique_type_id'];
-
-	if(isset($_POST['edit_info_p']))
-	{
-		
-	}
-	else if(isset($_POST['edit_info_u']))
-	{
-		
-	}
-	else if(isset($_POST['delete_info_p']))
-	{
-		$con->query("delete from unique_product where product_id='$edit_delete_product_id'");
-		$con->query("delete from products where product_id='$edit_delete_product_id'");
-	}
-	else if(isset($_POST['delete_info_u']))
-	{
-		$con->query("delete from unique_product where unique_type_id='$edit_delete_unique_type_id'");
-	}
-
-?>
 
 
 
