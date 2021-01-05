47540e2d9cd0e426082c9b71e408a26b828ed3c0
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 22:29:55 +0530

    Update edit_or_delete_from_db.php

--- admin/edit_or_delete_from_db.php
+++ admin/edit_or_delete_from_db.php
@@ -38,7 +38,7 @@
 		
 		$sql="update products set product_name='$fp_product_name',product_brand='$fp_product_brand',product_description='$fp_product_description',rating='$fp_product_rating' where product_id='$fp_product_id'";
 		
-		$res=$con->query();
+		$res=$con->query($sql);
 		
 		echo $con->error;
 		
