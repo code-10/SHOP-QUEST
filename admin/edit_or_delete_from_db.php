3b8ca146c9805325ba4a7917a9e5403504ebc8c6
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 22:40:41 +0530

    Update edit_or_delete_from_db.php

--- admin/edit_or_delete_from_db.php
+++ admin/edit_or_delete_from_db.php
@@ -36,7 +36,7 @@
 		echo $fp_product_description;echo "<br>";
 		echo $fp_product_rating;echo "<br>";
 		
-		$sql="update products set product_name='$fp_product_name',product_brand='$fp_product_brand',product_description="$fp_product_description",rating='$fp_product_rating' where product_id='$fp_product_id'";
+		$sql="update products set product_name='$fp_product_name',product_brand='$fp_product_brand',product_description='".mysqli_real_escape_string($con,$fp_product_description)."',rating='$fp_product_rating' where product_id='$fp_product_id'";
 		
 		$res=$con->query($sql);
 		
