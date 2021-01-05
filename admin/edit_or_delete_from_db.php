ddb6389b8516a9723991b56f3658ddf21bb02e85
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 15:07:36 +0530

    Update edit_or_delete_from_db.php

--- admin/edit_or_delete_from_db.php
+++ admin/edit_or_delete_from_db.php
@@ -17,6 +17,33 @@
         
 ?>
 
+<?php
+
+	$edit_delete_product_id=$_POST['edit_delete_product_id'];
+	$edit_delete_unique_type_id=$_POST['edit_delete_unique_type_id'];
+
+	if(isset($_POST['edit_info_p']))
+	{
+		
+	}
+	else if(isset($_POST['edit_info_u']))
+	{
+		
+	}
+	else if(isset($_POST['delete_info_p']))
+	{
+		$con->query("delete from unique_product where product_id='$edit_delete_product_id'");
+		$con->query("delete from products where product_id='$edit_delete_product_id'");
+                header("Location:check_db.php");
+	}
+	else if(isset($_POST['delete_info_u']))
+	{
+		$con->query("delete from unique_product where unique_type_id='$edit_delete_unique_type_id'");
+                header("Location:check_db.php");
+	}
+
+?>
+
 
 
 <body>
