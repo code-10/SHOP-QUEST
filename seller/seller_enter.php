79fd5f236d97ca447d1939f1f77b3e318b89645f
manikr17 <63992542+manikr17@users.noreply.github.com>
2021-01-05 19:42:48 +0530

    Added "my products" button

--- seller/seller_enter.php
+++ seller/seller_enter.php
@@ -154,7 +154,8 @@
       	    <div class="text-center m-4">
             	<a class="btn btn-primary" href="seller_enter.php?sell_a_product=yes" role="button">Sell a product</a>
             	<a class="btn btn-primary" href="seller_enter.php?my_sell_requests=yes" role="button">My sell requests</a>
-	   	 	</div>
+		<a class="btn btn-primary" href="seller_products.php?my_sell_requests=yes" role="button">My products</a>
+	   </div>
 	
       <?php } else if($sell_a_product=="yes") { ?>
       		
