bc98de37427d9a6b0450740cda51ebbfc3477079
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 15:02:23 +0530

    Create edit_or_delete_from_db.php

---
+++ admin/edit_or_delete_from_db.php
@@ -0,0 +1,63 @@
+<?php
+        include_once '../header.php';
+        session_start();
+        include '../libraries/chocolates.php';
+        $category=Array();
+        $con = getCon();  
+       
+        /*if(!(isset($_SESSION['user_name']))||($_SESSION['user_name']!="root"))
+      	{
+            header("Location:../index.php");
+            die(); 
+      	} */
+
+
+        
+        
+        
+?>
+
+
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="../index.php" class="navbar-brand">ShopShop</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+            <a href="../pages/about.php" class="nav-item nav-link">About</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['user_name'])){
+                    echo '<a href="../pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o"> '.$_SESSION['user_name'].'</i></a>';
+                    echo '<a href="../products/cart.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
+                    echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
+                }
+                else{
+                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
+                }
+            ?>
+        </div>
+    </div>
+</nav>
+
+     
+        
+ <div class="jumbotron">
+        <div class="text-center">
+              <h4>Edit or Delete Database Details</h4>
+        </div>
+    </div>       
+   
+        
+        
+  
+  
+  
+</body>
+
+<?php include_once '../footer.php'; ?>
