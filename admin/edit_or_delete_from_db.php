54f596047219ccb5a2ad87d28eff536110867e76
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 20:49:21 +0530

    Update edit_or_delete_from_db.php

--- admin/edit_or_delete_from_db.php
+++ admin/edit_or_delete_from_db.php
@@ -36,8 +36,7 @@
 		echo $fp_product_description;echo "<br>";
 		echo $fp_product_rating;echo "<br>";
 		
-		$edit_this_product_id=$_POST['edit_this_product_id'];
-		$con->query("update products set product_name='$fp_product_name',product_brand='$fp_product_brand',product_description='$fp_product_description',rating='$fp_product_rating' where product_id='$edit_this_product_id'");
+		$con->query("update products set product_name='$fp_product_name',product_brand='$fp_product_brand',product_description='$fp_product_description',rating='$fp_product_rating' where product_id='$fp_product_id'");
 		
 		header("Location:check_db.php");		
 	}
@@ -211,7 +210,6 @@
         <label for="inputproduct_rating">product rating</label>
         <input type="number" class="form-control" id="inputproduct_rating" placeholder="product rating" name="product_rating" value="<?=$p_edit_product_rating[0]?>" required>
     </div>
-    <input type="hidden" name="edit_this_product_id" value="<?=$edit_delete_product_id?>" />
     <button type="submit" name="edit_product" class="btn btn-dark">Edit product</button>
     </form> 	
 
