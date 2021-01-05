10a4f38d3a0b9d9ee0cc13bd92a13ddb393921bf
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-05 15:18:23 +0530

    Update edit_or_delete_from_db.php

--- admin/edit_or_delete_from_db.php
+++ admin/edit_or_delete_from_db.php
@@ -44,6 +44,45 @@
 
 ?>
 
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="../index.php" class="navbar-brand">ShopQuest</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+            <a href="../pages/about.php" class="nav-item nav-link">About</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['user_name'])){
+                    echo '<a href="../pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o"> '.$_SESSION['user_name'].'</i></a>';
+                    echo '<a href="../products/cart.php" class="nav-item nav-link active"><i class="fa fa-shopping-cart"></i></a>';
+                    echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
+                }
+                else{
+                    echo '<a href="../sign_in/sign_in.php" class="nav-item nav-link">Sign in</a>';
+                }
+            ?>
+        </div>
+    </div>
+</nav>
 
+     
+        
+ <div class="jumbotron">
+        <div class="text-center">
+              <h4>Edit or Delete Database Details</h4>
+        </div>
+    </div>       
+   
+        
+        
+  
+  
+  
+</body>
 
+<?php include_once '../footer.php'; ?>
