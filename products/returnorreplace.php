0bbfeadf0452053a79ae58b2094e8fc4210afcf1
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-06 12:05:51 +0530

    Update returnorreplace.php

--- products/returnorreplace.php
+++ products/returnorreplace.php
@@ -10,7 +10,14 @@
     if(isset($_POST['submit_return_or_replace']))
     {
         $reason=$_POST['reason'];
-        echo $reason;
+	$unique_type_id_rar=$_POST['unique_type_id_rar'];
+	$order_id_rar=$_POST['order_id_rar'];
+	$quantity_rar=$_POST['quantity_rar'];
+	    
+	echo $reason; echo "<br>";
+	echo $unique_type_id_rar; echo "<br>";
+	echo $order_id_rar; echo "<br>";
+	echo $quantity_rar; echo "<br>";
     
     }
   
