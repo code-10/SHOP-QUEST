74d045a6364e94cf4f4adceda8e660abb5a36ac3
manikr17 <63992542+manikr17@users.noreply.github.com>
2021-01-06 19:52:28 +0530

    Update db_update.php

--- seller/db_update.php
+++ seller/db_update.php
@@ -17,6 +17,6 @@
     $con->query("update store_info set quantity='$quantity', approved=0 where store_info_id='$store_info_id'");
     
     
-    header("Location:seller_enter.php?my_sell_requests=yes&&aprstatus=0");
+   
     
 ?>
