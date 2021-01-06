0f668d3475a136ba1ff1ae25afa7c3706ef71984
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-07 00:48:27 +0530

    Update user_request.php

--- admin/user_request.php
+++ admin/user_request.php
@@ -96,7 +96,7 @@
 	
 	<?php for($i=0;$i<$n;$i++) { ?>
 		
-		<div class="card">
+		<div class="card m-4">
   			<h5 class="card-header"><?=$rar_user_name[$i]?></h5>
   			<div class="card-body">
     				<p class="card-text">product name : <?=$rar_product_name[$i]?></p>
@@ -110,6 +110,11 @@
 				<p class="card-text">product descriptiom : <?=$rar_product_description[$i]?></p>
 				<p class="card-text">quantity : <?=$rar_quantity[$i]?></p>
 				<p class="card-text">seller_user_name : <?=$rar_seller_user_name[$i]?></p>
+				
+				<a href='#' class="btn btn-primary btn-sm m-2">Replace</a>
+				<a href='#' class="btn btn-primary btn-sm m-2">Return</a>
+				<a href='#' class="btn btn-danger btn-sm m-2">Reject</a>
+				
   			</div>
 		</div>
 	
