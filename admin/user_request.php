7bc50c0c32acff445bac1a42f14e0244828b49e6
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-06 20:02:54 +0530

    Update user_request.php

--- admin/user_request.php
+++ admin/user_request.php
@@ -21,7 +21,7 @@
 
 <body>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a href="../index.php" class="navbar-brand">ShopShop</a>
+    <a href="../index.php" class="navbar-brand">ShopQuest</a>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
