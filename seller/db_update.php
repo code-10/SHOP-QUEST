030afb3bd0c2830030c232fbb70b8af38a3d3001
manikr17 <63992542+manikr17@users.noreply.github.com>
2021-01-06 20:24:07 +0530

    approve=quantity

--- seller/db_update.php
+++ seller/db_update.php
@@ -15,7 +15,7 @@
 
 		$con=getCon();
     
-    $con->query("update store_info set quantity='$quantity', approved=0 where store_info_id='$store_info_id'");
+    $con->query("update store_info set approved='$quantity' where store_info_id='$store_info_id'");
 	
 header("Location:seller_enter.php?my_sell_requests=yes&&aprstatus=0");
     
