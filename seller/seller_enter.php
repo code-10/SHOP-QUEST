7d72ae6f0d41b2e815cd57b5c3584f6aef00a597
Manoj Kumar <manojkumar333619@gmail.com>
2021-01-24 18:22:22 +0530

    Update seller_enter.php

--- seller/seller_enter.php
+++ seller/seller_enter.php
@@ -153,7 +153,7 @@
 	
       	    <div class="text-center m-4">
             	<a class="btn btn-primary" href="seller_enter.php?sell_a_product=yes" role="button">Sell a product</a>
-            	<a class="btn btn-primary" href="seller_enter.php?my_sell_requests=yes&&aprstatus=0" role="button">My sell requests</a>
+            	<a class="btn btn-primary" href="seller_enter.php?my_sell_requests=yes&&show_stat=0" role="button">My sell requests</a>
 	   </div>
 	
       <?php } else if($sell_a_product=="yes") { ?>
