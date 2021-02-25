cd60fd624b469f54ae41831eba1d00ec2f40afe7
Manoj Kumar <manojkumar333619@gmail.com>
2021-02-25 20:14:54 +0530

    Update seller_enter.php

--- seller/seller_enter.php
+++ seller/seller_enter.php
@@ -265,7 +265,7 @@
 	
 	<div class="text-center m-4">
             <a <?php if($show_stat==0) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="seller_enter.php?my_sell_requests=yes&&show_stat=0" role="button">Pending<span class="badge badge-light ml-2"><?=$res0;?></span></a>
-	    <a <?php if($show_stat==1) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="seller_enter.php?my_sell_requests=yes&&show_stat=1" role="button">Approved <span class="badge badge-light ml-2 mr-2"><?=$res1;?></span> - Add Stock <span class="badge badge-warning ml-2"><?=$res4?>/<?=$res1;?></span> - Add Variant <span class="badge badge-info ml-2"><? echo"0"; ?>/<? echo"0"; ?></span></a>
+	    <a <?php if($show_stat==1) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="seller_enter.php?my_sell_requests_approved=yes&&show_stat=1" role="button">Approved <span class="badge badge-light ml-2 mr-2"><?=$res1;?></span> - Add Stock <span class="badge badge-warning ml-2"><?=$res4?>/<?=$res1;?></span> - Add Variant <span class="badge badge-info ml-2"><? echo"0"; ?>/<? echo"0"; ?></span></a>
             <a <?php if($show_stat==2) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="seller_enter.php?my_sell_requests=yes&&show_stat=2" role="button">Rejected<span class="badge badge-light ml-2"><?=$res2;?></span></a>
 	</div>
 			
@@ -285,10 +285,7 @@
     						<p class="card-text">product brand  : <?=$product_brand[$k]?></p>
     						<p class="card-text">product description : <?=$product_description[$k]?></p>
 
-					
-    				<? if($approved[$k]==1) { ?>			
-							
-					
+			
     				<? } else if($approved[$k]==2){ ?>
 					<p class="card-text">price : <?=$price[$k]?></p>
     					<p class="card-text">color : <?=$color[$k]?></p>
@@ -311,7 +308,14 @@
 				
  		 <?php $c++;$cc++; } ?>
 	
+      <?php if($my_sell_requests_approved=="yes") { ?>
+	
+	echo "intial working";	
+	
       <?php } ?>
+	
+	
+	
   
 
       
