8be433d87d88acf8df20cb564dfbbc2471c47ab8
Manoj Kumar <manojkumar333619@gmail.com>
2021-02-28 11:33:12 +0530

    Update sell_request.php

--- admin/sell_request.php
+++ admin/sell_request.php
@@ -11,9 +11,9 @@
       	{
         	header("Location:../index.php");
             die(); 
-      	}
+      	}*/
 
-      	$sell_request_main=$_GET['sell_request_main'];*/
+      	$sell_request_main=$_GET['sell_request_main'];
 	
 	
 
@@ -56,8 +56,41 @@
 	<a class="btn btn-dark ml-4" href="admin_enter.php?admin_enter_main=yes" role="button"><i class="fa fa-arrow-circle-left mr-2"></i>Back to main menu</a>
             
   
+  	<?php
+	
+		if($sell_request_main=="yes") {
+	
+	?>
   
-  
+	
+	<?php
+	
+		$sql0 = "select * from store_info where approved=0";
+		$res0i = $con->query($sql0);
+		$res0 = $res0i->num_rows;
+		//echo $res1;echo "<br>";
+	
+		$sql1 = "select * from store_info where approved=1";
+		$res1i = $con->query($sql1);
+		$res1 = $res1i->num_rows;
+	
+		$sql2 = "select * from store_info where approved=2";
+		$res2i = $con->query($sql2);
+		$res2 = $res2i->num_rows;
+	
+	
+	?>
+	
+	
+	
+	<div class="text-center m-4">
+            <a <?php if($status==0) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="sell_request.php?sell_request_main=yes&&status=0" role="button">Pending<span class="badge badge-light ml-2"><?=$res0;?></span></a>
+	    <a <?php if($status==1) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="sell_request.php?sell_request_main=yes&&status=1" role="button">Approved<span class="badge badge-light ml-2"><?=$res1;?></span></a>
+            <a <?php if($status==2) { ?> class="btn btn-dark m-2" <?php } else { ?> class="btn btn-primary m-2" <?php } ?> href="sell_request.php?sell_request_main=yes&&status=2" role="button">Rejected<span class="badge badge-light ml-2"><?=$res2;?></span></a>
+	</div>
+	
+	
+	<?php } ?>
   
   
   
