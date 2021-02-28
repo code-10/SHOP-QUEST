775dcce1ad484815af62faa9f061f98ba19e6e82
Manoj Kumar <manojkumar333619@gmail.com>
2021-02-28 13:25:34 +0530

    Update sell_request.php

--- admin/sell_request.php
+++ admin/sell_request.php
@@ -377,7 +377,66 @@
 	<?php } else if($admin_verify_product=="yes") { ?>
 	
         	<?php  
-			echo "working";
+			if(isset($_POST['verify_product']))
+      			{
+            			$con=getCon();
+
+ 				$subcategoryid=$_POST['sub_cat_id'];
+      				$productid=$_POST['product_id'];
+      				$productname=$_POST['product_name'];
+      				$productbrand=$_POST['product_brand'];
+      				$productdescription=$_POST['product_description'];
+      				$price=$_POST['price'];
+      				$quantity=$_POST['qty'];
+      				$color=$_POST['color'];
+      				$size=$_POST['size'];
+      				$sellername=$_POST['seller_name'];
+      				$rating=$_POST['rating'];          
+
+     
+  
+      				//For Store info
+      				$approved=$_POST['approve'];
+      				$storeinfoid=$_POST['storeinfoid'];
+      				$store_info_id=$_POST['store_info_id'];
+      				//$done=1;      
+
+				$user=$_SESSION['user_name'];
+      				//for updating store info
+      				$sql1="update store_info set approved=1 where store_info_id='$storeinfoid'";
+				
+      				$sql2="insert into products(product_id,product_name,sub_cat_id,product_brand,product_description,rating) values('".mysqli_real_escape_string($con,$product_id_c_use)."','".mysqli_real_escape_string($con,$productname)."','".mysqli_real_escape_string($con,$subcategoryid)."','".mysqli_real_escape_string($con,$productbrand)."','".mysqli_real_escape_string($con,$productdescription)."','".mysqli_real_escape_string($con,$rating)."')";
+				
+      				$sql3="insert into unique_product(product_id,price,quantity,seller_user_name,color,size) values('".mysqli_real_escape_string($con,$product_id_c_use)."','".mysqli_real_escape_string($con,$price)."','".mysqli_real_escape_string($con,$quantity)."','".mysqli_real_escape_string($con,$sellername)."','".mysqli_real_escape_string($con,$color)."','".mysqli_real_escape_string($con,$size)."')";
+      
+ 				$sql4="update store_info set store_product_id='".mysqli_real_escape_string($con,$product_id_c_use)."',admin_sub_category='".mysqli_real_escape_string($con,$subcategoryid)."',admin_product_name='".mysqli_real_escape_string($con,$productname)."',admin_product_brand='".mysqli_real_escape_string($con,$productbrand)."',admin_product_description='".mysqli_real_escape_string($con,$productdescription)."',admin_price='".mysqli_real_escape_string($con,$price)."',admin_quantity='".mysqli_real_escape_string($con,$quantity)."',admin_color='".mysqli_real_escape_string($con,$color)."',admin_size='".mysqli_real_escape_string($con,$size)."'";  
+	    
+				$sql5="update store_info set store_unique_type_id = (select unique_type_id from unique_product where product_id='$product_id_c_use' and seller_user_name='$sellername') where store_info_id='$storeinfoid'";
+	
+	      
+      				if($con->query($sql2)===True)
+      				{
+        				if($con->query($sql3)===True)
+        				{
+          					if($con->query($sql1)===True)
+         				 	{
+	   						if($con->query($sql4)===True)
+							{	
+								if($con->query($sql5)===True)
+								{
+              								header("Location:sell_request.php?sell_request_main=yes");
+                							die();
+								}
+						        }
+        					}					
+      					}
+				}
+      				else
+      				{
+        				echo "something went wrong"; 
+      				}  
+            
+      			}
 		?>
 	
 	<?php } ?>
